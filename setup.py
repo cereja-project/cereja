aadc1b079236e2624ef84ef7659b3b29a66f1474
Joab <jlsn1@ifal.edu.br>
2019-09-17 11:19:44 -0300

    Setup: Updated project setup

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="jlsneto-jlsneto",
+    name="jlsneto",
     version="0.0.1",
     author="Joab Leite",
-    author_email="jlsn1@ifal.edu.br;leitejoab@gmail.com",
+    author_email="jlsn1@ifal.edu.br",
     description="My Utils",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -15,7 +15,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU GENERAL PUBLIC LICENSE",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
