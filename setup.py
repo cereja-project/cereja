d6f7843d9a1d5af7ef2e42c343e13b517e47e5c2
jlsneto <leitejoab151631>
2019-09-18 21:30:40 -0300

    Version: Alter version of package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jlsneto",
-    version="0.0.6",
+    version="0.1.0",
     author="Joab Leite",
     author_email="jlsn1@ifal.edu.br",
     description="My Utils",
