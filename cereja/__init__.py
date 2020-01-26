3e5318f91133e4b19cb8b6e8ab5aa02657424bef
Joab <jlsn1@ifal.edu.br>
2020-01-25 22:28:25 -0300

    Version: fixes current release version

--- cereja/__init__.py
+++ cereja/__init__.py
@@ -2,9 +2,9 @@ from cereja.utils import get_version_pep440_compliant
 from . import utils
 from importlib import import_module
 
-VERSION = "1.0.1.final.0"
+VERSION = "1.0.2.final.0"
 
-__version__ = get_version_pep440_compliant()
+__version__ = get_version_pep440_compliant(VERSION)
 
 cj_modules_dotted_path = utils.import_string('cereja.conf.cj_modules_dotted_path')
 
