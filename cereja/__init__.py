0eed05cb205e2233148d7126fe0f10e0e91355c8
Joab <jlsn1@ifal.edu.br>
2020-01-25 22:21:23 -0300

    Version: release 1.0.1

--- cereja/__init__.py
+++ cereja/__init__.py
@@ -2,7 +2,7 @@ from cereja.utils import get_version_pep440_compliant
 from . import utils
 from importlib import import_module
 
-VERSION = "1.0.1.alpha.0"
+VERSION = "1.0.1"
 
 __version__ = get_version_pep440_compliant()
 
