82b849b9782cb1db23439bf0d9f6e7d946be82d4
Joab Leite S. Neto <jlsn1@ifal.edu.br>
2020-02-29 03:42:15 -0300

    Update files.py

--- cereja/files.py
+++ cereja/files.py
@@ -1,6 +1,7 @@
 import os
 
 from cereja.path import get_base_dir
+from cereja.display import Progress
 
 __all__ = []  # is'nt public https://www.python.org/dev/peps/pep-0008/#id50
 
@@ -17,13 +18,32 @@ def _open_file(file_path: str, mode='r'):
 _line_sep = f"{os.linesep}"
 
 
-def _crlf_to_lf(path: str):
+def _replace_end_line(file_path: str, old, new):
+    with open(file_path, 'r', encoding='utf-8') as fp:
+        lines = ''.join(fp.readlines()).replace(old, new)
+    with open(file_path, 'w+') as fp:
+        fp.write(lines)
+
+def _walk_dirs_and_replace(dir_path, old, new):
+    for root_dir, _, files in os.walk(dir_path):
+        with cj.Progress(root_dir) as prog:
+            for file_ in files:
+                file_path = os.path.join(root_dir, file_)
+                _replace_end_line(file_path, old, new)
+
+def crlf_to_lf(path_: str):
     """
-    use os.walk
-    :param path:
+    :param path_:
     :return:
     """
-    pass
+    _walk_dirs_and_replace(path_, '\r\n', '\n')
+
+def lf_to_crlf(path_: str):
+    """
+    :param path_:
+    :return:
+    """
+    _walk_dirs_and_replace(path_, '\n', '\r\n')
 
 
 def _to_lf(path: str):
