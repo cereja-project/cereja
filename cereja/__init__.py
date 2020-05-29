05f9af7fb8062d19fab8e8d2ec4cd9ca94d9ad0b
Joab <jlsn1@ifal.edu.br>
2020-05-29 19:50:32 -0300

    PostRelease: Updtade version

--- cereja/__init__.py
+++ cereja/__init__.py
@@ -25,7 +25,7 @@ from cereja.utils import get_version_pep440_compliant
 from . import utils
 from importlib import import_module
 
-VERSION = "1.1.4.final.1"
+VERSION = "1.1.4.final.2"
 
 __version__ = get_version_pep440_compliant(VERSION)
 
