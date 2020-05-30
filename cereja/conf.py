afbf7592c72b39546b767fcfaf38480ba829a82d
Joab <jlsn1@ifal.edu.br>
2020-05-30 19:05:13 -0300

    Config: Create basic config

--- cereja/conf.py
+++ cereja/conf.py
@@ -23,11 +23,71 @@ SOFTWARE.
 
 import sys
 import logging
+from abc import abstractmethod, ABCMeta
+
 from .utils import get_version_pep440_compliant
 
-__all__ = []
+__all__ = ['_BasicConfig']
+
 # using by utils.module_references
 _exclude = ["console_logger", "cj_modules_dotted_path"]
+
+
+class _BasicConfig(metaclass=ABCMeta):
+    def __init__(self, hook=None, **kwargs):
+        self._fields = ()
+        self._listen = hook
+        self.set_config(**kwargs)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.get()})'
+
+    def __setattr__(self, key, value):
+        if hasattr(self, '_locked'):
+            if self._locked is True:
+                er = 'This is a critical config. You can use <set_config> method.'
+                raise ValueError(er)
+        super().__setattr__(key, value)
+
+    def __unlock(self):
+        super().__setattr__('_locked', False)
+
+    def __lock(self):
+        self._fields = tuple(self.get().keys())
+        super().__setattr__('_locked', True)
+
+    def set_config(self, **kwargs) -> None:
+        self._before(kwargs)
+        self.__unlock()
+        for k, i in kwargs.items():
+            self.__setattr__(k, i)
+        self.__lock()
+        if self._listen is not None:
+            self._listen()
+
+    @abstractmethod
+    def _before(self, new_config: dict):
+        for k in new_config:
+            if k not in self._fields and self._fields:
+                raise ValueError(f"<{k}> not in config attrs {self._fields}")
+
+    def get(self, item=None):
+        config = {}
+        if item is None:
+            for attr_ in dir(self):
+                if attr_.startswith('_'):
+                    continue
+                obj = self.__getattribute__(attr_)
+                if callable(obj):
+                    continue
+                config[attr_] = obj
+            return config
+        if hasattr(self, item):
+            obj = self.__getattribute__(item)
+            if not callable(obj):
+                return obj
+        return {}
+
 
 # Used to add the functions of each module at the root
 cj_modules_dotted_path = ['cereja.arraytools', 'cereja.conf', 'cereja.decorators', 'cereja.path',
