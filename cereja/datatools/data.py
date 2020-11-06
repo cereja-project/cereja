922edd525cfeabc152cafaf3c8c9511d99d0b0bc
jlsneto <leitejoab@gmail.com>
2020-11-06 09:06:14 -0300

    Tokenizer: add tokenizer

--- cereja/datatools/data.py
+++ cereja/datatools/data.py
@@ -20,14 +20,18 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-
+import logging
+import secrets
 from collections import Counter
-from typing import Optional, Sequence, Dict, Any
+from typing import Optional, Sequence, Dict, Any, List, Union, Tuple
 
-from cereja.arraytools import is_iterable
+from cereja.arraytools import is_iterable, is_sequence, get_shape
 from cereja.filetools import JsonFile
+from cereja.utils import invert_dict
 
-__all__ = ["Freq"]
+logger = logging.Logger(__name__)
+
+__all__ = ["Freq", 'Tokenizer']
 
 
 class Freq(Counter):
@@ -96,3 +100,121 @@ class Freq(Counter):
     def to_json(self, path_, probability=False, **kwargs):
         content = self.probability if probability else self
         JsonFile(path_=path_, data=content).save(**kwargs)
+
+
+class Tokenizer:
+    _n_unks = 10
+    __unks = dict({f'{{{i}}}': i for i in range(_n_unks)})
+    __unks.update(invert_dict(__unks))
+
+    def __init__(self, data: List[str]):
+        self._lang_data = self.language_data(data)
+        self._index_to_word = dict(enumerate(self._lang_data.words, self._n_unks))
+        self._word_to_index = invert_dict(self._index_to_word)
+        self._temp_unks = dict()
+        self._hash = self.new_hash  # default
+
+    @classmethod
+    def language_data(cls, data):
+        # because circular import
+        # TODO: fix me
+        from cereja.datatools.pln import LanguageData
+        return LanguageData(data)
+
+    @property
+    def unks(self):
+        return self.__unks
+
+    @property
+    def unk_memory(self):
+        return self._temp_unks
+
+    @property
+    def hash_default(self):
+        return self._hash
+
+    @property
+    def new_hash(self):
+        return secrets.token_hex(7)
+
+    def word_index(self, word: str) -> int:
+        return self._word_to_index.get(word)
+
+    def index_word(self, index: int) -> str:
+        return self._index_to_word.get(index, self.unks.get(index))
+
+    @classmethod
+    def normalize(cls, data) -> List[Any]:
+        data = data or []
+        if isinstance(data, str):
+            return [data]
+        elif isinstance(data, (int, float, bytes)):
+            return [data]
+        assert is_sequence(data), TypeError(f'this data type is not supported, try sending a {str}, {list} or tuple')
+        return data
+
+    def _encode(self, data: List[str], hash__):
+        if hash__ not in self._temp_unks:
+            self._temp_unks[hash__] = {}
+        n = 0
+        result = []
+        for word in self._lang_data.preprocess(data):
+            index = self.word_index(word)
+            if index is None:
+                unk = f'{{{n % self._n_unks}}}'
+                index = self.unks.get(unk)
+                self._temp_unks[hash__].update({(n, f"{word}"): unk})
+                n += 1
+            result.append(index)
+        return result
+
+    def encode(self, data: Union[str, List[str]]) -> List[Tuple[List[int], str]]:
+        """
+        Encodes values in a sequence of numbers
+
+        the hash is used to decode "unks" in the correct order.
+
+        e.g:
+            tokenizer = Tokenizer(data=['i like it', 'my name is Joab', 'hello'])
+            sequences = tokenizer.encode(data=['hello my friend, how are you?', 'my name is mário'])
+            # [([9, 7, 15, 10, 16, 5], 'e9bc59cb0d1564'), ([7, 13, 2, 15], '9f92140ebb0e19')]
+
+        """
+        result = []
+        for sentence in self.normalize(data):
+            assert isinstance(sentence, str), "send List[str] or str"
+            __hash = self.new_hash
+            result.append((self._encode(sentence.split(), __hash), __hash))
+        return result
+
+    def decode(self, data: Union[List[int], int]):
+        return [self.index_word(index) for index in self.normalize(data)]
+
+    def to_json(self, path_: str):
+        JsonFile(path_, self._index_to_word).save(exist_ok=True)
+
+    def replace_unks(self, sentence: str, hash_):
+        assert isinstance(sentence, str), 'expected a string.'
+        try:
+            _temp_unks = self._temp_unks[hash_].copy()
+            if not _temp_unks:
+                return sentence
+            for (indx, word), unk in _temp_unks.items():  # cannot have an exception
+                sentence = sentence.replace(unk, word)
+                self._temp_unks[hash_].pop((indx, word))
+            self._temp_unks.pop(hash_)
+        except KeyError as err:
+            logger.error(
+                    msg=f"{err}: There's something wrong open please issue "
+                        f"https://github.com/jlsneto/cereja/issues/new?template=bug-report.md")
+        return sentence
+
+
+if __name__ == '__main__':
+    tokenizer = Tokenizer(data=['i like it', 'my name is Joab', 'hello'])
+    sequences = tokenizer.encode(data=['hello my friend, how are you?', 'my name is mário'])
+    decoded = []
+    for encoded_sequence, hash_ in sequences:
+        decoded.append(tokenizer.replace_unks(' '.join(tokenizer.decode(encoded_sequence)), hash_))
+
+    print(decoded)
