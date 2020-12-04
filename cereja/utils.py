9e32c03bf99dc96da3b0684b31b1a0cd75801324
jlsneto <leitejoab@gmail.com>
2020-12-03 22:06:57 -0300

    utils: run_on_terminal

--- cereja/utils.py
+++ cereja/utils.py
@@ -470,3 +470,15 @@ def memory_of_this(obj):
 def memory_usage(n_most=10):
     return sorted(map(lambda x: (x[0], sys.getsizeof(x[1])), globals().items()), key=lambda x: x[1], reverse=True)[
            :n_most]
+
+
+def run_on_terminal(cmd: str):
+    try:
+        subprocess.run(
+                cmd,
+                shell=True, stdout=subprocess.PIPE).check_returncode()
+    except subprocess.CalledProcessError as err:
+        err_output = err.output.decode()
+        raise Exception(f"{err}:{err_output}")
+    except Exception as err:
+        raise Exception(err)
