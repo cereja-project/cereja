7932fe6a91519c03ad8353eb7d61d3ebf8ec308a
jlsneto <leitejoab@gmail.com>
2021-05-01 23:28:05 -0300

    version: update version

--- cereja/__init__.py
+++ cereja/__init__.py
@@ -46,7 +46,7 @@ from . import mathtools
 from cereja.mathtools import *
 from . import experimental
 
-VERSION = "1.3.7.final.0"
+VERSION = "1.3.8.final.0"
 
 __version__ = get_version_pep440_compliant(VERSION)
 
