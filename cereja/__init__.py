a5a08e95c1256cfb859124039872f914eb94e603
jlsneto <leitejoab@gmail.com>
2021-08-02 12:08:28 -0300

    release: v1.5.1

--- cereja/__init__.py
+++ cereja/__init__.py
@@ -47,7 +47,7 @@ from .mathtools import *
 from . import experimental
 from ._requests import request
 
-VERSION = "1.5.0.final.0"
+VERSION = "1.5.1.final.0"
 
 __version__ = get_version_pep440_compliant(VERSION)
 
