9b9d7c93846cbc155f6a65edead0bf4be2176025
jlsneto <leitejoab@gmail.com>
2024-06-01 07:53:05 -0300

    refactor: rename CompanyDetails to Share

--- cereja/scraping/b3.py
+++ cereja/scraping/b3.py
@@ -10,78 +10,100 @@ STOCK_EXCHANGES_CONFIG = {
 }
 
 
-def _parse_b3_url_query(query):
-    query_base64_str = base64_encode(query).decode()
-    return f"{STOCK_EXCHANGES_CONFIG['B3']}/{query_base64_str}"
+class StockExchangeConfig:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(StockExchangeConfig, cls).__new__(cls)
+            cls._instance._initialize()
+        return cls._instance
+
+    def _initialize(self):
+        # Initializes the configurations for the stock exchange APIs
+        self.config = STOCK_EXCHANGES_CONFIG
+
+    def get_config(self, exchange):
+        # Returns config for a specific stock exchange
+        return self.config.get(exchange.upper())
 
 
 class Share:
-    name: str
-    cnpj: str
-    trading_code: str
-    code_cvm: str
-    date_listing: str  # dd/mm/yyyy
-    bdr_type: str
-    is_bdr: bool
-    market_indicator: int
-
-    language: str = "pt-br"
-
-    def __init__(self, trading_code, exchange="B3"):
-        assert exchange in STOCK_EXCHANGES_CONFIG, f"Exchange {exchange} is currently not supported."
-
-        self.trading_code = trading_code.upper()  # TODO: need validation
-        self._exchange = exchange.upper()
-        self._api_config = STOCK_EXCHANGES_CONFIG[self._exchange]
+    def __init__(self, trading_code, exchange="B3", language="pt-br"):
+        self.trading_code = trading_code.upper()
+        self.language = language
+        self.config = StockExchangeConfig().get_config(exchange)
         self._head_lines = []
 
-        if self._exchange == "B3":
-            try:
-                # Regist info
-                query = {"language": self.language, "pageNumber": 1, "pageSize": 20, "company": self.trading_code}
-                # need convert base64
-                query = base64_encode(query).decode()
-                query_url_parsed = f"{self._api_config['base_api_url']}/{self._api_config['regist_info_endpoint']}/{query}"
-                resp = request.get(query_url_parsed, timeout=30)
-                if resp.code == 200:
-                    results = resp.json()["results"]
-                    if len(results) >= 1:
-                        reg_info = results[0]
-                        self.code_cvm = reg_info["codeCVM"]
-                        self.name = reg_info["companyName"]
-                        self.cnpj = get_zero_mask(int(reg_info["cnpj"]), 14)  # fix cnpj left-zero
-                        self.segment = reg_info["segment"]
-                        self.market_indicator = reg_info["marketIndicator"]
-                        self.bdr_type = reg_info["typeBDR"]
-                        self.date_listing = reg_info["dateListing"]
-            except Exception as err:
-                raise Exception(f"Erro ao processar dados de registro. {err}")
+        if not self.config:
+            raise ValueError(f"Exchange {exchange} is currently not supported.")
 
-            try:
-                # headlines info
-                query = {'agency':         self.market_indicator, 'dateInitial': '2024-05-02',
-                         'dateFinal':      '2024-06-01',
-                         'issuingCompany': ''.join(char for char in self.trading_code if not char.isnumeric())}
-                # need convert base64
-                query = base64_encode(query).decode()
-                query_url_parsed = f"{self._api_config['base_api_url']}/{self._api_config['head_lines_endpoint']}/{query}"
-                resp = request.get(query_url_parsed, timeout=30).json()
-                for headline in resp:
-                    self._head_lines.append({
-                        "headline": headline["headline"],
-                        "date":     headline["dateTime"],
-                        "url":      headline["url"]
-                    })
-            except Exception as err:
-                raise Exception(f"Erro ao processar dados de eventos. {err}")
+        self._initialize_share_info()
+        self._initialize_headlines()
+
+    def _get(self, url_parsed, timeout=30) -> dict:
+        response = request.get(url_parsed, timeout=timeout)
+        if response.code == 200:
+            return response.json()
+        raise ConnectionRefusedError(response.data)
+
+    def _initialize_share_info(self):
+
+        try:
+            # Fetches and processes the share information from the API
+            query = {"language": self.language, "pageNumber": 1, "pageSize": 20, "company": self.trading_code}
+            query_encoded = base64_encode(query).decode()
+            url = f"{self.config['base_api_url']}/{self.config['regist_info_endpoint']}/{query_encoded}"
+            response = self._get(url)
+            results = response.get("results", [])
+
+            if results:
+                reg_info = results[0]
+                self.code_cvm = reg_info["codeCVM"]
+                self.name = reg_info["companyName"]
+                self.cnpj = get_zero_mask(int(reg_info["cnpj"]), 14)
+                self.segment = reg_info["segment"]
+                self.market_indicator = reg_info["marketIndicator"]
+                self.bdr_type = reg_info["typeBDR"]
+                self.date_listing = reg_info["dateListing"]
+
+        except Exception as err:
+            raise Exception(f"Erro ao processar dados de registro. {err}")
+
+    def _initialize_headlines(self):
+
+        try:
+            # Fetches and processes the headlines related to the share from the API
+            query = {
+                'agency':         self.market_indicator,
+                'dateInitial':    '2024-05-02',
+                'dateFinal':      '2024-06-01',
+                'issuingCompany': ''.join(char for char in self.trading_code if not char.isnumeric())
+            }
+            query_encoded = base64_encode(query).decode()
+            url = f"{self.config['base_api_url']}/{self.config['head_lines_endpoint']}/{query_encoded}"
+            response = self._get(url)
+
+            for headline in response:
+                self._head_lines.append({
+                    "headline": headline["headline"],
+                    "date":     headline["dateTime"],
+                    "url":      headline["url"]
+                })
+
+        except Exception as err:
+            raise Exception(f"Erro ao processar dados de eventos. {err}")
 
     @property
-    def exchange(self) -> str:
-        return self._exchange
+    def exchange(self):
+        # Returns the stock exchange associated with the share
+        return self.config.get("exchange")
 
     @property
     def is_bdr(self):
+        # Checks if the share is a BDR (Brazilian Depositary Receipt)
         return bool(self.bdr_type)
 
     def __repr__(self):
+        # String representation of the Share instance
         return f"{self.trading_code}(cnpj={self.cnpj}, name={self.name})"
