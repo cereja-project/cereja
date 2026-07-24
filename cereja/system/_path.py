49d6276eb2514411e2d989f8799f2b9dc22aabf8
Joab Leite <leitejoab@gmail.com>
2026-07-23 23:20:18 -0300

    refactor(logging): change info logs to debug for path validation checks

--- cereja/system/_path.py
+++ cereja/system/_path.py
@@ -224,17 +224,17 @@ class Path(os.PathLike):
                 return
             part_split = part.name.split(".")
             if part_split[-1] == ".":
-                logger.info(
+                logger.debug(
                         f"It is not common to use dot <{part.name}> in the end of name."
                 )
                 break
             if (part.suffix or part_split[-1] == ".") and i > 0:
-                logger.info(
+                logger.debug(
                         f"It is not common to use dot in the middle or end of directory name <{part.name}>"
                 )
                 break
             if len(part_split) > 2:
-                logger.info(f"<{part.name}> has more dot than usual.")
+                logger.debug(f"<{part.name}> has more dot than usual.")
             part = part.parent
         self._verified.add(str(self.__path.parent))
 
