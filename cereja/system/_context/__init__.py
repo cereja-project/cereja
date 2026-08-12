d749c700c266e0503a4b6c0bad46dfe91cdcc058
Joab Leite S. Neto <leitejoab@gmail.com>
2026-08-12 09:20:40 -0300

    refactor: isolate context search orchestration

--- cereja/system/_context/__init__.py
+++ cereja/system/_context/__init__.py
@@ -1,1 +1,5 @@
 """Private components for bounded textual context search."""
+
+from .models import ContextResponse, ContextResult, ContextSnippet, SkippedFile
+from .query import context_response_to_dict
+from .search import list_text_context, search_text_context
