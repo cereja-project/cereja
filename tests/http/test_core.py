40529391f9ceb4d6f2a352afcaf1d1f3cddda7d2
Joab Leite S. Neto <leitejoab@gmail.com>
2026-09-12 19:00:14 -0300

    test: define failing HTTP core milestone

---
+++ tests/http/test_core.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class CoreContractsPlaceholderTest(unittest.TestCase):
+    def test_placeholder(self):
+        self.fail("HTTP core contracts are not implemented yet")
+
+
+if __name__ == "__main__":
+    unittest.main()
