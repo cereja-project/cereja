d860cf5069fca19ce4362fc56070ac45dede34da
Joab Leite S. Neto <leitejoab@gmail.com>
2026-09-24 01:43:50 -0300

    Bump version to 2.3.0

--- cereja/_version.py
+++ cereja/_version.py
@@ -5,5 +5,5 @@ public version formatter produces the same PEP 440 value. Release metadata must
 not depend on a runtime Git subprocess or on an installed distribution.
 """
 
-VERSION = "2.2.0.final.0"
-__version__ = "2.2.0"
+VERSION = "2.3.0.final.0"
+__version__ = "2.3.0"
